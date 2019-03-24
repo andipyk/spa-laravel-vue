e98af5d247187e443f7ea2ae23c7fcdbebb004b8
unknown <andisyafrianda@gmail.com>
2019-03-24 18:19:07 +0700

    final

--- frontend/src/views/Articles.vue
+++ frontend/src/views/Articles.vue
@@ -1,10 +1,38 @@
 <template>
-    <h1>Article List</h1>
+    <div>
+        <div v-for="item in articles" :key="item.id">
+            <h1>{{ item.title }}</h1>
+            <p>{{ item.body }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'articles'
+    name: 'articles',
+    data(){
+        return {
+            articles: null
+        }
+    },
+    created() {
+        this.getArticles()
+    },
+    methods: {
+        getArticles(){
+            // console.log('tes')
+            axios.get('/api/articles')
+                    .then( (res) => {
+                        const {data:{data}} = res
+                        this.articles = data
+                        console.log(res)
+                        
+                        })
+                    .catch((err) => console.error())
+        }
+    },
 }
 </script>
 
